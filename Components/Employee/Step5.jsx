45f4ef0e5ab7a8bfdd9eddf948d0bb512a57afde
Vikas Ukani <vikasukani5@gmail.com>
2021-06-17 01:47:39 +0530

    DONE:  Step 5 Process Data has been stored..

--- Components/Employee/Step5.jsx
+++ Components/Employee/Step5.jsx
@@ -4,6 +4,7 @@ import { useAxios } from "hooks";
 import { useEffect, useState } from "react";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
 import { useToasts } from "react-toast-notifications";
+import { setCookie } from "services";
 import { uploadImageService } from "services/image";
 import { getMasterByCode } from "services/masters";
 
@@ -17,14 +18,16 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
 
   const [documentTypeList, setDocumentTypeList] = useState();
 
-
   const [documents, setDocuments] = useState();
-  const [documentYearError, setDocumentYearError] = useState();
-
+  const [documentTypeError, setDocumentTypeError] = useState({ 0: null });
 
   const { addToast } = useToasts();
-  const documentFieldClone = { document_id: null, document_number: null, front_image_id: null, back_image_id: null }
-
+  const documentFieldClone = {
+    document_id: null,
+    document_number: null,
+    front_image_id: null,
+    back_image_id: null
+  }
 
   const {
     register,
@@ -47,7 +50,7 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
   const updateIndexedDocument = (id, index) => {
     setDocuments({ ...documents, [index]: id });
     /** Remove Errors after select */
-    setDocumentYearError({ ...documentYearError, [index]: null });
+    setDocumentTypeError({ ...documentTypeError, [index]: null });
   };
 
   const fetchDocumentsTypes = async () => {
@@ -58,7 +61,6 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
     } else {
       addToast(error.message, { appearance: 'error', autoDismiss: false })
     }
-    console.log("response, statusCode, error", data, statusCode, error);
   }
 
   const uploadImage1 = async (e) => {
@@ -123,25 +125,22 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
     setDefaultFileList2(fileList);
   };
 
-
-
   /** Final Save */
   const onSubmit = (inputData) => {
-    console.log("inputData", documents[0], inputData);
-    // if (!specialization || specialization.length == 0) {
-    //   setSpecializationError("The select any specialization.!");
-    //   return false;
-    // }
+    inputData.documents.forEach((element, idx) => {
+      if (documentTypeError && documentTypeError[idx] !== null) {
+        setDocumentTypeError({ ...documentTypeError, [idx]: " Document type is required" })
+      }
+    });
+
     let UpdatedData = {
-      ...input,
+      ...currentData,
       ...inputData,
     };
-
     UpdatedData.documents.forEach((documentL, idx) => {
-      documentL.document = documents[idx];
+      documentL.document_id = documents[idx];
     });
-    // console.log("Final UpdatedData", UpdatedData);
-    // setCookie("step5", UpdatedData);
+    setCookie("step5", UpdatedData);
     console.log("Saved step5", UpdatedData);
     // goToNextStep(UpdatedData);
   };
@@ -160,7 +159,7 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
       <form role="form" action="index.html" className="login-box">
         <div className="tab-content" id="main_form">
           <div className="tab-pane active" role="tabpanel" id="step1">
-            <div className="row">
+            <div className="">
               <label className="top-label w-100 mb-0 mt-1">
                 {documentsFields.fields.length < 5 && (
                   <a
@@ -176,35 +175,32 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
               {documentsFields.fields.map((item, index) => {
                 return (
                   <>
-                    <div className="col-xl-6">
-                      <div className="form-group">
+                  <div key={index} className="d-flex row mt-0 ">
+                    <div className="col-xl-6 ">
+                      <div className="form-group  ">
                         <label className="top-label">Select Document</label>
                         <Select
                           className=""
                           placeholder="Select document type."
                           style={{ width: "100%" }}
-                          name={`document_id`}
+                          name={`documents.${index}.document_id`}
+                          control={control}
                           value={stepInput?.documents?.[index]?.document_id}
-                        >
-                          {/*   onChange={(id) =>
+                          defaultValue={stepInput?.documents?.[index]?.document_id}
+                          onChange={(id) =>
                             updateIndexedDocument(id, index)
-                          } */}
-                          {/* value={stepInput?.documents?.[index]?.document_id} */}
-                          {/* defaultValue={stepInput?.documents?.[index]?.document_id} */}
+                          }
+                        >
+
                           {documentTypeList?.map(dL => (
                             <Select.Option key={dL.id} value={dL.id}>
                               {dL.name}
                             </Select.Option>
                           ))}
                         </Select>
-                        {/* {errors?.documents && errors?.documents[index] && (
+                        {documentTypeError?.[index] && (
                           <p className=" text-danger">
-                            {errors?.documents[index].document_id.message}
-                          </p>
-                        )} */}
-                        {documentYearError?.[index] && (
-                          <p className=" text-danger">
-                            {documentYearError[index]}
+                            {documentTypeError[index]}
                           </p>
                         )}
                       </div>
@@ -213,81 +209,68 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
                     <div className="col-xl-5">
                       <div className="form-group">
                         <label className="top-label">Document Number</label>
-                        <Controller
-                          render={({ field }) => (
-                            <input
-                              type="text"
-                              className="form-control form-control-lg mb-25 mt-25"
-                              {...field}
-                            />
-                          )}
-                          placeholder="Enter Document Number"
+                        <input
+                          type="text"
+                          className="form-control form-control-lg mb-25 mt-25"
                           name={`documents.${index}.document_number`}
                           defaultValue={stepInput?.documents?.[index]?.document_number}
                           control={control}
                           {...register(`documents.${index}.document_number`, {
-                            required: "Document number is required.",
+                            required: "Document is required.",
                           })}
                         />
                         {errors?.documents && errors?.documents[index] && (
                           <p className=" text-danger">
                             {errors?.documents[index].document_number.message}
                           </p>
-                        )}{/* {errors.last_name && (
-                    <span className="mt-5 text-danger">
-                      {errors.last_name.message}
-                    </span>
-                  )} */}
+                        )}
                       </div>
                     </div>
-                    <div className=" pt-2 ">
+                    <div className="col-xl-1 pt-2 ">
                       {documentsFields.fields.length > 1 && (
                         <a
                           onClick={() => documentsFields.remove(index)}
-                          className="btn btn-danger custom_btn add-icon mt-50   "
+                          className="btn btn-danger custom_btn add-icon mt-1 float-left   "
                         >
                           <i className="fa fa-minus "></i>
                         </a>
                       )}
                     </div>
-                  </>
-                );
-              })}
-              <div className="col-lg-6">
-                <div className="form-group mt-1 mb-3">
-                  <label className="w-100 mb-1">
-                    Upload Document
-                    {/* <a href="#" className="btn btn-primary add-icon">
+                    <div className="col-xl-6">
+                      <div className="form-group   mb-1">
+                        <label className="w-100  ">
+                          Upload Document
+                          {/* <a href="#" className="btn btn-primary add-icon">
                       <i className="fa fa-plus"></i>
                     </a> */}
-                  </label>
-                  <div className="d-inline-flex ">
-                    <Upload
-                      accept="image/*"
-                      customRequest={(e) => uploadImage1(e)}
-                      defaultFileList={defaultFileList1}
-                      onChange={handleOnChange1}
-                      listType="picture-card"
-                      onPreview={onPreview1}
-                    >
-                      {defaultFileList1.length >= 1 ? null : (
-                        <div>Front Image</div>
-                      )}
-                    </Upload>
-                    <Upload
-                      accept="image/*"
-                      customRequest={(e) => uploadImage2(e)}
-                      defaultFileList={defaultFileList2}
-                      onChange={handleOnChange2}
-                      listType="picture-card"
-                      onPreview={onPreview2}
-                    >
-                      {defaultFileList2.length >= 1 ? null : (
-                        <div>Back Image</div>
-                      )}
-                    </Upload>
-                  </div>
-                  {/* <div className="custom-file">
+                        </label>
+                        <div className="d-inline-flex ">
+                          <Upload
+                            accept="image/*"
+                            customRequest={(e) => uploadImage1(e)}
+                            defaultFileList={defaultFileList1}
+                            onChange={handleOnChange1}
+                            listType="picture-card"
+                            onPreview={onPreview1}
+                          >
+                            {defaultFileList1.length >= 1 ? null : (
+                              <div>Front Image</div>
+                            )}
+                          </Upload>
+                          <Upload
+                            accept="image/*"
+                            customRequest={(e) => uploadImage2(e)}
+                            defaultFileList={defaultFileList2}
+                            onChange={handleOnChange2}
+                            listType="picture-card"
+                            onPreview={onPreview2}
+                          >
+                            {defaultFileList2.length >= 1 ? null : (
+                              <div>Back Image</div>
+                            )}
+                          </Upload>
+                        </div>
+                        {/* <div className="custom-file">
                     <input
                       type="file"
                       className="form-control custom-file-input"
@@ -298,8 +281,14 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
                       Upload
                     </label>
                   </div> */}
-                </div>
-              </div>
+                      </div>
+                    </div>
+                  
+                  </div>
+                  <hr className="pb-2 mt-0"  />
+                </>
+                );
+              })}
             </div>
 
             <div className="row">
@@ -311,8 +300,8 @@ const Step5 = ({ currentData, goToNextStep, goToPrevStep }) => {
                   <i className="fa fa-angle-left"></i> BACK
                 </a>
                 <a
-                  onClick={handleSubmit(onSubmit)}
                   className="float-right mx-1 btn btn-pill mb-sm-0 mb-2 text_theme_primary   default_gradient"
+                  onClick={handleSubmit(onSubmit)}
                 >
                   NEXT <i className="fa fa-angle-right"></i>
                 </a>
